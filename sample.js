ad724bf5a488f955a4dc3cb09a0d4301c860a016
SWETHASARAVANANVS <swetha.cs21@bitsathy.ac.in>
2023-08-21 10:33:03 +0530

    second commit

--- sample.js
+++ sample.js
@@ -5,7 +5,7 @@ var db = connect('127.0.0.1:27017/madMen'),
 print('* Database created');
 
 //create the names collection and add documents to it
-db.names.insert({'name' : 'Don Draper'});
+db.names.insert({'name' : 'Don Drapmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmr'});
 db.names.insert({'name' : 'Peter Campbell'});
 db.names.insert({'name' : 'Betty Draper'});
 db.names.insert({'name' : 'Joan Harris'});
